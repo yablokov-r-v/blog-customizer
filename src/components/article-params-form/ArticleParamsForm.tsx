0bd6a558b163eed0212acd3f3351055f629b2860
Roman Yablokov <yablokov.r.v@yandex.ru>
2024-12-16 01:11:42 +0300

    Сдача проектной работы 10 спринта от 15.12.2024

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,131 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { useState, useRef } from 'react';
+import clsx from 'clsx';
+import { Select } from 'src/ui/select/Select';
+import { RadioGroup } from 'src/ui/radio-group/RadioGroup';
 import styles from './ArticleParamsForm.module.scss';
+import { OptionType } from 'src/constants/articleProps';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import { Separator } from 'src/ui/separator/Separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	fontFamilyOptions: OptionType[];
+	selectedFontFamily: OptionType;
+	handleFontFamilyChange: (option: OptionType) => void;
+
+	fontSizeOptions: OptionType[];
+	selectedFontSize: OptionType;
+	handleFontSizeChange: (option: OptionType) => void;
+
+	fontColors: OptionType[];
+	selectedFontColor: OptionType;
+	handleFontColorChange: (option: OptionType) => void;
+
+	backgroundColors: OptionType[];
+	selectedBackgroundColor: OptionType;
+	handleBackgroundColorChange: (option: OptionType) => void;
+
+	contentWidthArr: OptionType[];
+	selectedContentWidth: OptionType;
+	handleContentWidthChange: (option: OptionType) => void;
+
+	handleApplyStyles: () => void;
+	handleResetStyles: () => void;
+};
+
+export const ArticleParamsForm: React.FC<ArticleParamsFormProps> = ({
+	fontFamilyOptions,
+	selectedFontFamily,
+	handleFontFamilyChange,
+
+	fontSizeOptions,
+	selectedFontSize,
+	handleFontSizeChange,
+
+	fontColors,
+	selectedFontColor,
+	handleFontColorChange,
+
+	backgroundColors,
+	selectedBackgroundColor,
+	handleBackgroundColorChange,
+
+	contentWidthArr,
+	selectedContentWidth,
+	handleContentWidthChange,
+
+	handleApplyStyles,
+	handleResetStyles,
+}) => {
+	const [isAsideOpen, setIsAsideOpen] = useState(false);
+	const asideRef = useRef(null);
+
+	const handleArrowBottonClick = () => {
+		setIsAsideOpen(!isAsideOpen);
+	};
+
+	useOutsideClickClose({
+		isOpen: isAsideOpen,
+		rootRef: asideRef,
+		onClose: () => setIsAsideOpen(false),
+		onChange: setIsAsideOpen,
+	});
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isAsideOpen} onClick={handleArrowBottonClick} />
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isAsideOpen,
+				})}>
 				<form className={styles.form}>
+					<Select
+						options={fontFamilyOptions}
+						selected={selectedFontFamily}
+						onChange={handleFontFamilyChange}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={selectedFontSize}
+						onChange={handleFontSizeChange}
+						title='Размер шрифта'
+					/>
+					<Select
+						options={fontColors}
+						selected={selectedFontColor}
+						onChange={handleFontColorChange}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						options={backgroundColors}
+						selected={selectedBackgroundColor}
+						onChange={handleBackgroundColorChange}
+						title='Цвет фона'
+					/>
+					<Select
+						options={contentWidthArr}
+						selected={selectedContentWidth}
+						onChange={handleContentWidthChange}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='button'
+							type='clear'
+							onClick={handleResetStyles}
+						/>
+						<Button
+							title='Применить'
+							htmlType='button'
+							type='apply'
+							onClick={handleApplyStyles}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, useEffect } from 'react';
 import clsx from 'clsx';
-
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-
+import {
+	defaultArticleState,
+	OptionType,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from './constants/articleProps';
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
@@ -13,19 +19,131 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [articleState, setArticleState] = useState(defaultArticleState);
+	const [tempState, setTempState] = useState(defaultArticleState);
+	const [selectedFontFamily, setSelectedFontFamily] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [selectedFontSize, setSelectedFontSize] = useState(
+		defaultArticleState.fontSizeOption
+	);
+	const [selectedFontColor, setSelectedFontColor] = useState(
+		defaultArticleState.fontColor
+	);
+	const [selectedBackgroundColor, setSelectedBackgroundColor] = useState(
+		defaultArticleState.backgroundColor
+	);
+	const [selectedContentWidth, setSelectedContentWidth] = useState(
+		defaultArticleState.contentWidth
+	);
+
+	useEffect(() => {
+		document.documentElement.style.setProperty(
+			'--font-family',
+			articleState.fontFamilyOption.value
+		);
+		document.documentElement.style.setProperty(
+			'--font-size',
+			articleState.fontSizeOption.value
+		);
+		document.documentElement.style.setProperty(
+			'--font-color',
+			articleState.fontColor.value
+		);
+		document.documentElement.style.setProperty(
+			'--container-width',
+			articleState.contentWidth.value
+		);
+		document.documentElement.style.setProperty(
+			'--bg-color',
+			articleState.backgroundColor.value
+		);
+	}, [articleState]);
+
+	const handleFontFamilyChange = (option: OptionType) => {
+		setTempState((prevState) => ({
+			...prevState,
+			fontFamilyOption: option,
+		}));
+		setSelectedFontFamily(option);
+	};
+
+	const handleFontSizeChange = (option: OptionType) => {
+		setTempState((prevState) => ({
+			...prevState,
+			fontSizeOption: option,
+		}));
+		setSelectedFontSize(option);
+	};
+
+	const handleFontColorChange = (option: OptionType) => {
+		setTempState((prevState) => ({
+			...prevState,
+			fontColor: option,
+		}));
+		setSelectedFontColor(option);
+	};
+
+	const handleBackgroundColorChange = (option: OptionType) => {
+		setTempState((prevState) => ({
+			...prevState,
+			backgroundColor: option,
+		}));
+		setSelectedBackgroundColor(option);
+	};
+
+	const handleContentWidthChange = (option: OptionType) => {
+		setTempState((prevState) => ({
+			...prevState,
+			contentWidth: option,
+		}));
+		setSelectedContentWidth(option);
+	};
+
+	const handleApplyStyles = () => {
+		setArticleState(tempState);
+	};
+
+	const handleResetStyles = () => {
+		setTempState(defaultArticleState);
+		setSelectedFontFamily(defaultArticleState.fontFamilyOption);
+		setSelectedFontSize(defaultArticleState.fontSizeOption);
+		setSelectedFontColor(defaultArticleState.fontColor);
+		setSelectedBackgroundColor(defaultArticleState.backgroundColor);
+		setSelectedContentWidth(defaultArticleState.contentWidth);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				fontFamilyOptions={fontFamilyOptions}
+				selectedFontFamily={selectedFontFamily}
+				handleFontFamilyChange={handleFontFamilyChange}
+				fontSizeOptions={fontSizeOptions}
+				selectedFontSize={selectedFontSize}
+				handleFontSizeChange={handleFontSizeChange}
+				fontColors={fontColors}
+				selectedFontColor={selectedFontColor}
+				handleFontColorChange={handleFontColorChange}
+				backgroundColors={backgroundColors}
+				selectedBackgroundColor={selectedBackgroundColor}
+				handleBackgroundColorChange={handleBackgroundColorChange}
+				contentWidthArr={contentWidthArr}
+				selectedContentWidth={selectedContentWidth}
+				handleContentWidthChange={handleContentWidthChange}
+				handleApplyStyles={handleApplyStyles}
+				handleResetStyles={handleResetStyles}
+			/>
 			<Article />
 		</main>
 	);
